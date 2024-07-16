6d5615c3f948931e681d5ecae167d996ebeb4332
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-07-16 16:00:50 +0100

    Sketch out overall structure

--- grammar.js
+++ grammar.js
@@ -2,6 +2,44 @@ module.exports = grammar({
   name: "dbml",
 
   rules: {
-    document: $ => optional(/\w+/)
+    document: $ => repeat($._definition),
+
+    _definition: $ => choice(
+      // $.project,
+      // $.table,
+      // $.enum,
+      // $.reference,
+      // $.table_group,
+      // $.comment,
+      // $.note,
+    ),
+
+    // project: $ => seq(
+
+    // ),
+
+    // table: $ => seq(
+
+    // ),
+
+    // enum: $ => seq(
+
+    // ),
+
+    // reference: $ => seq(
+
+    // ),
+
+    // table_group: $ => seq(
+
+    // ),
+
+    // comment: $ => seq(
+
+    // ),
+
+    // note: $ => seq(
+
+    // ),
   }
 })
